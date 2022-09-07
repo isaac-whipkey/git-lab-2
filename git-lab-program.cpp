4d99db53cf4e28991ff01390754c11d338bdf07a
Isaac Whipkey <iw721221@ohio.edu>
2022-09-07 17:37:31 -0400

    This is my program

---
+++ git-lab-program.cpp
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Isaac Whipkey
+ *        Date: 9/7/2022
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
